b9eedab4ae84e0472419e17f70de8a2149072517
Dalitso Banda <dalitsohb@gmail.com>
2017-04-26 02:29:29 -0400

    added enet

--- cifarseg.py
+++ cifarseg.py
@@ -8,6 +8,7 @@ It gets down to 0.65 test logloss in 25 epochs, and down to 0.55 after 50 epochs
 '''
 
 from __future__ import print_function
+import keras
 from keras.datasets import cifar10
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
@@ -15,54 +16,57 @@ from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.utils import np_utils
 from model import get_frontend, add_softmax
+from PIL import Image
 import numpy as np
 from keras.preprocessing.image import (
     load_img, img_to_array,
     flip_axis)
 
-n_samples = 200
-batch_size = 32
+n_samples = 10
+batch_size = 10
 nb_classes = 151
-nb_epoch = 200
+nb_epoch = 1
 data_augmentation = True
 
 # input image dimensions
-img_rows, img_cols = 500, 500
+img_rows, img_cols = 384,384
 
 # The CIFAR10 images are RGB.
 img_channels = 3
 
 # output dimensions
-mask_rows, mask_cols = 16, 16
+mask_rows, mask_cols = 384,384
 
 # The data, shuffled and split between train and test sets:
-X_train = np.zeros((n_samples,3,500,500))
-y_train = np.zeros((n_samples,16*16,nb_classes))
-X_test = np.zeros((n_samples,3,500,500))
-y_test = np.zeros((n_samples,16*16,nb_classes))
+X_train = np.zeros((n_samples,3,img_rows, img_cols))
+y_train = np.zeros((n_samples,mask_rows*mask_cols,nb_classes))
+X_test = np.zeros((n_samples,3,img_rows, img_cols))
+y_test = np.zeros((n_samples,mask_rows*mask_cols,nb_classes))
 
 for i in range(1,n_samples):
     if i%100 ==0:
         print("loaded ", i)
-    ade_dir = '/Users/dalitsobanda/github/segmentation/ADEChallengeData2016/'
+    ade_dir = '/home/dalitso/segmentation/ADEChallengeData2016/'
 
     train_img_fname = '{}/images/training/ADE_train_{:08d}.jpg'.format(ade_dir, i)
-    img = load_img(train_img_fname, grayscale=False, target_size= (500,500))
+    img = load_img(train_img_fname, grayscale=False, target_size= (img_rows, img_cols))
+    # img.show(title=train_img_fname)
     x = img_to_array(img, data_format='channels_first')
     X_train[i] = x
 
     train_mask_fname = '{}/annotations/training/ADE_train_{:08d}.png'.format(ade_dir, i)
-    img = load_img(train_mask_fname, grayscale=True, target_size= (16,16))
+    img = load_img(train_mask_fname, grayscale=True, target_size= (mask_rows, mask_cols))
+    # img.show(title=train_img_fname)
     y = img_to_array(img, data_format='channels_first')
     y_train[i] = np_utils.to_categorical(y,nb_classes)
 
     val_img_fname = '{}/images/validation/ADE_val_{:08d}.jpg'.format(ade_dir, i)
-    img = load_img(val_img_fname, grayscale=False, target_size= (500,500))
+    img = load_img(val_img_fname, grayscale=False, target_size= (img_rows, img_cols))
     x = img_to_array(img, data_format='channels_first')
     X_test[i] = x
     
     val_mask_fname = '{}/annotations/validation/ADE_val_{:08d}.png'.format(ade_dir, i)
-    img = load_img(val_mask_fname, grayscale=True, target_size= (16,16))
+    img = load_img(val_mask_fname, grayscale=True, target_size= (mask_rows, mask_cols))
     y = img_to_array(img, data_format='channels_first')
     y_test[i] = np_utils.to_categorical(y,nb_classes)
 
@@ -76,13 +80,16 @@ print(X_test.shape[0], 'test samples')
 # Y_test = np_utils.to_categorical(y_test, nb_classes).reshape((2000,151,16,16))
 
 model = add_softmax(
-    get_frontend(500, 500))
+    get_frontend(img_rows, img_cols))
 print(model.summary())
 
 
+# initiate RMSprop optimizer
+opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
+
 # Let's train the model using RMSprop
 model.compile(loss='categorical_crossentropy',
-              optimizer='rmsprop',
+              optimizer=opt,
               metrics=['accuracy'])
 
 X_train = X_train.astype('float32')
@@ -123,3 +130,13 @@ else:
                         steps_per_epoch=X_train.shape[0],
                         epochs=nb_epoch,
                         validation_data=(X_test, y_test))
+
+    choice = np.random.choice(X_train.shape[0], 3)
+    pred = model.predict(X_train[choice])
+    print(pred.shape)
+    for i in range(pred.shape[0]):
+        arr = pred[i].argmax(1).reshape((mask_rows, mask_cols))
+        arr = np.uint8(arr)
+        im = Image.fromarray(arr)
+        im.show()
+
---
+++ models/enet.py
@@ -0,0 +1,42 @@
+from keras.engine.topology import Input
+from keras.models import Model
+from keras.layers.core import Activation, Reshape, Dense
+from keras.utils import plot_model
+from models import encoder, decoder
+
+
+def transfer_weights(model, weights=None):
+    '''
+    Always trains from scratch; never transfers weights
+    '''
+    print('ENet has found no compatible pretrained weights! Skipping weight transfer...')
+    return model
+
+
+def autoencoder(nc, input_shape,
+                loss='categorical_crossentropy',
+                optimizer='adadelta'):
+    # data_shape = input_shape[0] * input_shape[1] if input_shape and None not in input_shape else None
+    data_shape = input_shape[0] * input_shape[1] if input_shape and None not in input_shape else -1  # TODO: -1 or None?
+    inp = Input(shape=(3,input_shape[0], input_shape[1]))
+    enet = encoder.build(inp)
+    enet = decoder.build(enet, nc=nc, in_shape=input_shape)
+
+    # enet = Reshape((data_shape, nc), input_shape=(input_shape[0], input_shape[1], nc))(enet)
+    # from keras import backend as K
+    # enet = K.reshape(enet, (data_shape, nc))
+    # print(K.int_shape(enet))
+    enet = Reshape((data_shape, nc))(enet)  # TODO: need to remove data_shape for multi-scale training
+    enet = Activation('softmax')(enet)
+    # print(enet, )
+
+    model = Model(inputs=inp, outputs=enet)
+
+    model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy', 'mean_squared_error'])
+    name = 'enet'
+    print(model.summary())
+    return model, name
+
+if __name__ == "__main__":
+    autoencoder, name = autoencoder(nc=151, input_shape=(512, 512))
+    # plot_model(autoencoder, to_file='{}.png'.format(name), show_shapes=True)
